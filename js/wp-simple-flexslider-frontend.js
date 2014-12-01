f9aca4de06354caeddfa2e471ed999dff859c27e
Fikri Rasyid <fikrirasyid@gmail.com>
2014-12-01 13:21:21 +0700

    Set useCSS into false
    Preventing weird flickering bug caused by CSS3 translate3d. Reference:
    http://stackoverflow.com/questions/13101811/flexslider-really-messing-up
    -my-webpage-in-chrome
    
    Further reading:
    http://stackoverflow.com/questions/15152470/chrome-rendering-issue-fixed
    -position-anchor-with-ul-in-body/15203880#15203880

--- js/wp-simple-flexslider-frontend.js
+++ js/wp-simple-flexslider-frontend.js
@@ -2,7 +2,8 @@ jQuery(document).ready(function($){
 	$('.wp-simple-flexslider').each(function(){
 		$(this).flexslider({
 			animation: "slide",
-			selector: ".slides > .slide-item"
+			selector: ".slides > .slide-item",
+			useCSS: false
 		});			
 	});
 });
